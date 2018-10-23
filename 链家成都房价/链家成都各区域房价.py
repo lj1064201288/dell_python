2d7b3b942dc5adbbc6e6e98bee7cf42643f1c0f8
lj1064201288 <1064201288@qq.com>
2018-10-24 03:38:36 +0800

    链家成都房价信息

---
+++ 链家成都房价/链家成都各区域房价.py
@@ -0,0 +1,120 @@
+import os
+import csv
+import math
+import pypinyin
+import requests
+from lxml import etree
+
+class Chengdu(object):
+
+    def __init__(self, locat):
+        self.locat = locat
+        self.page = 0
+
+        self.content = ''
+        self.url = 'https://cd.fang.lianjia.com/loupan/{0}/'.format(self.locat)
+
+    def get_end_page(self):
+        try:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                text = response.text
+                html = etree.HTML(text)
+                num = html.xpath('//div[@class="page-box"]/@data-total-count')[0]
+                end_page = math.ceil(int(num)/10)
+                self.page = end_page
+            else:
+                print('访问失败')
+        except Exception as e:
+            print(e)
+
+    def get_items(self):
+
+        for page in range(1, self.page+1):
+            url = self.url + 'pg' + str(page)
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    self.content = response.text
+                    yield self.content
+            except:
+                print("ContentError!!!")
+
+    def get_parse(self):
+        if self.get_items():
+            for info in self.get_items():
+                html = etree.HTML(info)
+                items = html.xpath('//div/ul[@class="resblock-list-wrapper"]/li')
+
+                for item in items:
+                    houses = []
+                    houses.append(item.xpath('./div[@class="resblock-desc-wrapper"]/div/a/text()')[0].strip())
+                    houses.append(item.xpath('./div[@class="resblock-desc-wrapper"]/div/span/text()')[0].strip())
+                    houses.append(item.xpath('./div[@class="resblock-desc-wrapper"]/div/span/text()')[1].strip())
+                    locat = item.xpath('.//div[@class="resblock-location"]/a/text()')
+                    houses.append('/'.join(item.xpath('.//div[@class="resblock-location"]/span/text()') + locat))
+                    area = item.xpath('.//div[@class="resblock-area"]/span/text()')
+                    if area:
+                        houses.append(area[0].strip())
+                    else:
+                        houses.append('没有说明')
+                    houses.append('/'.join(item.xpath('.//div[@class="resblock-tag"]/span/text()')))
+                    houses.append(''.join(item.xpath('.//div[@class="main-price"]/span/text()')))
+
+                    yield houses
+
+    def write_csv(self):
+
+        file_path =  '链家' + os.sep + self.locat + os.sep
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+        file = open(file_path + self.locat + '.csv', 'a')
+        writer = csv.writer(file)
+        writer.writerow(['名称', '类型', '状态', '地址', '面积', '标签', '价格'])
+        # content_list = list(self.get_parse())
+        # print(content_list)
+        # print(type(content_list))
+        content_generator = self.get_parse()
+        for content in content_generator:
+            print(content)
+            content = list(content)
+            writer.writerow(content)
+        #file.write(content)
+        file.close()
+
+
+def city_locat(city):
+    lianjia = Chengdu(city)
+    lianjia.get_end_page()
+    lianjia.get_items()
+    lianjia.get_parse()
+    lianjia.write_csv()
+
+def city(area):
+    pinyin_locat = pypinyin.pinyin(area, pypinyin.NORMAL)
+    locat = ''
+    for pinyin in pinyin_locat:
+        locat += ''.join(pinyin)
+    if area == '高新':
+        locat = locat + '7'
+    if area == '高新西':
+        locat = locat + '1'
+    citys = ['jinjiang','qingyang','wuhou','gaoxin7','chenghua','jinniu','tianfuxinqu','gaoxinxi1','shuangliu','wenjiang','pidou','longquanyi','xindou','qingbaijiang','jintang','dayi','pujiang','xinjin','pengzhou','qionglai','chongzhou1','doujiangyan','jianyang','tianfuxinqunanqu','deyang','leshan','meishan','ziyang']
+
+    if locat in citys:
+        index = citys.index(locat)
+        city_locat(citys[index])
+    else:
+        print('没有这个城市,请重新输入!')
+
+if __name__ == '__main__':
+
+    while True:
+        area = input('请输入需要获取的地区(输入q退出):')
+
+        if area == 'q':
+            break
+
+        city(area)
+
+
